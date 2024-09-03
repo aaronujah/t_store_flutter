c199c969abc69aafef53a8699e02537625e5e51f
VMC <aaronujah@gmail.com>
2024-09-03 23:37:01 +0100

    Feat: Setup project utilities and assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:t_store/utils/theme/theme.dart';
+import 'package:t_store/app.dart';
 
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'T Store',
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
 }
