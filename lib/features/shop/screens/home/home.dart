d034848ee8e01f2a2c76eb0b35d0a8feb98fd21e
VMC <aaronujah@gmail.com>
2024-09-15 01:10:36 +0100

    Feat: Add category slider and search bar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:t_store/common/widgets/texts/section_heading.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:t_store/features/shop/screens/home/widgets/home_category.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,7 +17,34 @@ class HomeScreen extends StatelessWidget {
           children: [
             TPrimaryHeaderContainer(
               child: Column(
-                children: [THomeAppbar()],
+                children: [
+                  THomeAppbar(),
+                  SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+                  TSearchContainer(
+                    text: 'Search in Store',
+                  ),
+                  SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        TSectionHeading(
+                          title: 'Popular Categories',
+                          showActionButton: false,
+                          textColor: Colors.white,
+                        ),
+                        SizedBox(
+                          height: TSizes.spaceBtwItems,
+                        ),
+                        THomeCategories()
+                      ],
+                    ),
+                  )
+                ],
               ),
             )
           ],
